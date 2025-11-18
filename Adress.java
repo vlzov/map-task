3d93219df35752ae5b64a39b8595423f3ead59cf
Vladimir <vlzov36@gmail.com>
2025-11-18 23:35:20 +0300

    Update Adress.java

--- Adress.java
+++ Adress.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Adress {
     private String country;
     private String city;
@@ -13,5 +15,16 @@ public class Adress {
 
     public String getCity() {
         return city;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(country, city);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Adress o = (Adress) obj;
+        return country.equals(o.country) &&  city.equals(o.city);
     }
 }
